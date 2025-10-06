02bee61d50b913238e0ff13643be270943e0d284
EricW8 <ericwei524@gmail.com>
2025-10-05 21:37:43 -0700

    firebase database added

--- app/src/main/java/com/example/chattranslation/LoginOtpActivity.java
+++ app/src/main/java/com/example/chattranslation/LoginOtpActivity.java
@@ -1,6 +1,8 @@
 package com.example.chattranslation;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginOtpActivity extends AppCompatActivity {
+    String phoneNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,12 @@ public class LoginOtpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_LONG).show();
+
+        Map<String,String> data = new HashMap<>();
+        FirebaseFirestore.getInstance().collection("test").add(data);
+
     }
 }
